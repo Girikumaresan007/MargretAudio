36e04551b966eec3cb31b9b2d94316293ce92498
Girikumaresan007 <girikumaresan007@gmail.com>
2026-01-28 11:54:37 +0530

    Update UI and responsive fixes

--- src/PlanDetails/PlanDetails.jsx
+++ src/PlanDetails/PlanDetails.jsx
@@ -138,21 +138,44 @@ const PlanDetails = () => {
               ⏱ Duration <br />
               <strong>8–10 hours</strong>
             </div>
-            <div className="pill">
-              👥 Coverage <br />
-              <strong>Up to 300 guests</strong>
-            </div>
+          <div className="pill">
+        <span className="pill-icon">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+             width="18"
+           height="18"
+            viewBox="0 0 24 24"
+           fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+         strokeLinecap="round"
+        strokeLinejoin="round"
+       >
+        <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2" />
+        <circle cx="9" cy="7" r="4" />
+        <path d="M23 21v-2a4 4 0 0 0-3-3.87" />
+        <path d="M16 3.13a4 4 0 0 1 0 7.75" />
+      </svg>
+    </span>
+
+       <div>
+         Coverage <strong>Up to 300 guests</strong>
+     </div>
+     </div>
+
           </div>
 
           <h3 className="Head">What's Included</h3>
 
           <div className="features">
             {detailedPlan.features.map((f, i) => (
-              <div className="feature" key={i}>
-                ✓ {f}
-              </div>
-            ))}
-          </div>
+           <div className="feature" key={i}>
+            <span>✓</span>
+          {f}
+           </div>
+             ))}
+           </div>
+
         </div>
 
         {/* RIGHT */}
